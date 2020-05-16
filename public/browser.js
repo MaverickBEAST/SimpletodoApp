dd25a55f5f6ae12bf954ed8c51a4cd1990838cb7
maverickBeast <banuka.lahiru@outlook.com>
2020-05-16 23:27:39 +0530

    firstcommit

---
+++ public/browser.js
@@ -0,0 +1,55 @@
+function itemTemplate(item) {
+    return `<li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
+    <span class="item-text">${item.text}</span>
+    <div>
+    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Delete</button>
+    </div>
+    </li>`
+  }
+// IPLR
+let ourHTML = items.map(function(item){
+    return itemTemplate(item)
+}).join('')
+document.getElementById("item-list").insertAdjacentHTML("beforeend",ourHTML)
+
+    //Create
+    let createField = document.getElementById("create-field")
+
+    document.getElementById("create-form").addEventListener("submit", function(e) {
+    e.preventDefault()
+    axios.post('/create-item', {text: createField.value}).then(function (response) {
+        // Create the HTML for a new item
+        document.getElementById("item-list").insertAdjacentHTML("beforeend", itemTemplate(response.data))
+        createField.value = ""
+        createField.focus()
+    }).catch(function() {
+        console.log("Please try again later.")
+    })
+    })
+    
+    //Delete
+    document.addEventListener("click", function(e) {
+    if(e.target.classList.contains("delete-me")){
+        if(confirm("Do yOu want to delete")){
+            axios.post('/delete-item',{id: e.target.getAttribute("data-id")}).then(function(){
+                e.target.parentElement.parentElement.remove();
+
+            }).catch(function(){
+                console.log("Deleted");
+            });  
+        }
+    }
+
+    //Update
+    if(e.target.classList.contains("edit-me")){
+        let userInput = prompt("Enter Edited Details", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML);
+        if(userInput){
+            axios.post('/update-item',{text: userInput, id: e.target.getAttribute("data-id")}).then(function(){
+                e.target.parentElement.parentElement.querySelector(".item-text").innerHTML = userInput;
+            }).catch(function(){
+                console.log("Edited");
+            }); 
+        }   
+    }
+})
